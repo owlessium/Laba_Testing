7510d9f89dbb1f9d8b9af9ce073d9a9253ba8262
Виталина Вельтман <euledesunglucks@gmail.com>
2023-05-25 03:33:30 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Laba_Testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label = new System.Windows.Forms.Label();
+            this.textBoxRadius = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.labelWarningRadius = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelArea = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNumCorners = new System.Windows.Forms.TextBox();
+            this.labelWarningCorners = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(450, 17);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox.MinimumSize = new System.Drawing.Size(350, 350);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.SizeChanged += new System.EventHandler(this.pictureBox_SizeChanged);
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            this.pictureBox.Resize += new System.EventHandler(this.pictureBox_Resize);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(14, 5);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(399, 20);
+            this.label.TabIndex = 1;
+            this.label.Text = "Введите радиус окружности (в см, но не более 10!):";
+            // 
+            // textBoxRadius
+            // 
+            this.textBoxRadius.Location = new System.Drawing.Point(14, 28);
+            this.textBoxRadius.Name = "textBoxRadius";
+            this.textBoxRadius.Size = new System.Drawing.Size(395, 26);
+            this.textBoxRadius.TabIndex = 2;
+            this.textBoxRadius.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxRadius_KeyPress);
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.button.Location = new System.Drawing.Point(83, 316);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(261, 73);
+            this.button.TabIndex = 3;
+            this.button.Text = "Построить фигуру";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.UseWaitCursor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // labelWarningRadius
+            // 
+            this.labelWarningRadius.AutoSize = true;
+            this.labelWarningRadius.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWarningRadius.ForeColor = System.Drawing.Color.DarkRed;
+            this.labelWarningRadius.Location = new System.Drawing.Point(60, 57);
+            this.labelWarningRadius.MaximumSize = new System.Drawing.Size(400, 0);
+            this.labelWarningRadius.Name = "labelWarningRadius";
+            this.labelWarningRadius.Size = new System.Drawing.Size(312, 44);
+            this.labelWarningRadius.TabIndex = 4;
+            this.labelWarningRadius.Text = "Допускается ввод только целых неотрицательных чисел";
+            this.labelWarningRadius.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Площадь заштрихованной фигуры:";
+            // 
+            // labelArea
+            // 
+            this.labelArea.AutoSize = true;
+            this.labelArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelArea.Location = new System.Drawing.Point(316, 419);
+            this.labelArea.Name = "labelArea";
+            this.labelArea.Size = new System.Drawing.Size(39, 20);
+            this.labelArea.TabIndex = 6;
+            this.labelArea.Text = "???";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(371, 419);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "см2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(387, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Введите количество лучей у звезды ( от 3 до 10):";
+            // 
+            // textBoxNumCorners
+            // 
+            this.textBoxNumCorners.Location = new System.Drawing.Point(18, 187);
+            this.textBoxNumCorners.Name = "textBoxNumCorners";
+            this.textBoxNumCorners.Size = new System.Drawing.Size(395, 26);
+            this.textBoxNumCorners.TabIndex = 9;
+            this.textBoxNumCorners.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxNumCorners_KeyPress);
+            // 
+            // labelWarningCorners
+            // 
+            this.labelWarningCorners.AutoSize = true;
+            this.labelWarningCorners.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWarningCorners.ForeColor = System.Drawing.Color.DarkRed;
+            this.labelWarningCorners.Location = new System.Drawing.Point(60, 216);
+            this.labelWarningCorners.MaximumSize = new System.Drawing.Size(400, 0);
+            this.labelWarningCorners.Name = "labelWarningCorners";
+            this.labelWarningCorners.Size = new System.Drawing.Size(312, 44);
+            this.labelWarningCorners.TabIndex = 10;
+            this.labelWarningCorners.Text = "Допускается ввод только целых неотрицательных чисел";
+            this.labelWarningCorners.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBoxNumCorners);
+            this.panel1.Controls.Add(this.labelWarningCorners);
+            this.panel1.Controls.Add(this.label);
+            this.panel1.Controls.Add(this.textBoxRadius);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.labelWarningRadius);
+            this.panel1.Controls.Add(this.labelArea);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(432, 487);
+            this.panel1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 505);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MinimumSize = new System.Drawing.Size(830, 560);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "Лабораторная работа по тестированию";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBoxRadius;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label labelWarningRadius;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelArea;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNumCorners;
+        private System.Windows.Forms.Label labelWarningCorners;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_Testing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DrawStarInCircle(pictureBox, 1*50, 5);
+        }
+
+        private void DrawStarInCircle(PictureBox pictureBox, int radius, int numCorners)
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+
+            pictureBox.Width = radius * 2 + 40;
+            pictureBox.Height = pictureBox.Width;
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                // Очистить pictureBox
+                g.Clear(Color.White);
+
+                // вычисляем координаты центра окружности
+                int centerX = pictureBox.Width / 2;
+                int centerY = pictureBox.Height / 2;
+
+                // создаем массив точек для построения звезды
+                PointF[] starPoints = new PointF[2 * numCorners];
+                double angle = 0;
+                double rCurr = 0;
+                for (int i = 0; i < 2 * numCorners; i++)
+                {
+                    angle = Math.PI / numCorners * i;
+                    rCurr = i % 2 == 0 ? radius : 0.4 * radius;
+                    starPoints[i] = new PointF(
+                        (float)(centerX + rCurr * Math.Sin(angle)),
+                        (float)(centerY - rCurr * Math.Cos(angle)));
+                }
+                using (GraphicsPath path = new GraphicsPath())
+                {
+                    path.AddPolygon(starPoints);
+                    path.AddEllipse(centerX - radius, centerY - radius, 2 * radius, 2 * radius);
+                    using (HatchBrush brush = new HatchBrush(HatchStyle.BackwardDiagonal, Color.Red, Color.Transparent))
+                    {
+                        g.FillPath(brush, path);
+                        g.DrawPath(Pens.Black, path);
+                    }
+                }
+
+                // Нарисовать звезду
+                g.DrawPolygon(Pens.Black, starPoints);
+
+                // Нарисовать окружность
+                g.DrawEllipse(Pens.Red, centerX - radius, centerY - radius, 2 * radius, 2 * radius);
+            }
+
+            pictureBox.Image = bmp;
+            // Отобразить изображение в pictureBox
+
+            labelArea.Text = FindAreaFigure(radius, numCorners).ToString();
+
+        }
+
+        private double FindAreaFigure(int radius, int numCorners)
+        {
+            double starArea = 0;
+            double circleArea = 0;
+            double angle = 2 * Math.PI / numCorners;
+            double radiusStar = radius * 0.4;
+            double side = 2 * radius * Math.Sin(angle / 2);
+            double p = 3 * side / 2;
+            starArea = numCorners * (Math.Sqrt(p * Math.Pow((p - side), 3)) - Math.Pow(side, 2));
+
+            circleArea = Math.PI * Math.Pow(radius, 2);
+
+            double area = Math.Round((circleArea - starArea) / (50*50), 2);
+
+            return area;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            List<String> values = GetValues();
+            int radius = int.Parse(values[0]) * 50;
+            int numCorners = int.Parse(values[1]);
+            DrawStarInCircle(pictureBox, radius, numCorners);
+        }
+
+        private List<String> GetValues()
+        {
+            List <String> values = new List<String>(2) { "1", "5" };
+            this.textBoxRadius.KeyPress += new KeyPressEventHandler(this.textBoxRadius_KeyPress);
+            this.textBoxNumCorners.KeyPress += new KeyPressEventHandler(this.textBoxNumCorners_KeyPress);
+            String valueRadius = textBoxRadius.Text;
+            String valueNumCorners = textBoxNumCorners.Text;
+
+            if ((valueNumCorners == null || valueNumCorners == "" || valueNumCorners == " " || valueNumCorners == "0") && (valueRadius == null || valueRadius == "" || valueRadius == " " || valueRadius == "0"))
+            {
+                labelWarningRadius.Text = "Вводимое число не должно быть не больше  10, по умолчанию установлен радиус 1";
+                labelWarningRadius.Visible = true;
+                labelWarningCorners.Text = "Вводимое число должно быть в диапазоне от 3 до 10, по умолчанию установлено 5";
+                labelWarningCorners.Visible = true;
+                values.Insert(0, "1");
+                values.Insert(1, "5");
+            }
+            else if (valueRadius == null || valueRadius == "" || valueRadius == " ")
+            {
+                labelWarningRadius.Text = "Вводимое число не должно быть не больше 10, по умолчанию установлен радиус 1";
+                labelWarningRadius.Visible = true;
+                if (int.Parse(valueNumCorners) <= 10 && int.Parse(valueNumCorners) >= 3)
+                { 
+                    values.Insert(0, "1");
+                    values.Insert(1, valueNumCorners);
+                }
+                else  
+                {
+                    labelWarningCorners.Text = "Вводимое число должно быть в диапазоне от 3 до 10, по умолчанию установлено 5";
+                    labelWarningCorners.Visible = true;
+                    values.Insert(0, "1");
+                    values.Insert(1, "5");
+                }
+            }
+            else if (valueNumCorners == "" || valueNumCorners == null || valueRadius == " ")
+            {
+                labelWarningCorners.Text = "Вводимое число должно быть в диапазоне от 3 до 10, по умолчанию установлено 5";
+                labelWarningCorners.Visible = true;
+                if (int.Parse(valueRadius) > 10)
+                {
+                    labelWarningRadius.Text = "Вводимое число не должно быть не больше  10, по умолчанию установлен радиус 1";
+                    labelWarningRadius.Visible = true;
+                    values.Insert(0, "1");
+                    values.Insert(1, "5");
+                }
+                else
+                {
+                    values.Insert(0, valueRadius);
+                    values.Insert(1, "5");
+                }
+
+            } else if (int.Parse(valueNumCorners) <= 10 && int.Parse(valueNumCorners) >= 3)
+            {
+                if (int.Parse(valueRadius) <= 10)
+                {
+                    values.Insert(0, valueRadius);
+                    values.Insert(1, valueNumCorners);
+                } else
+                {
+                    labelWarningRadius.Text = "Вводимое число не должно быть не больше 10, по умолчанию установлен радиус 1";
+                    labelWarningRadius.Visible = true;
+                    values.Insert(0, "1");
+                    values.Insert(1, valueNumCorners);
+                }
+            } else if (int.Parse(valueRadius) <= 10)
+            {
+                if (int.Parse(valueNumCorners) <= 10 && int.Parse(valueNumCorners) >= 3 && int.Parse(valueRadius) > 10)
+                {
+                    values.Insert(0, valueRadius);
+                    values.Insert(1, valueNumCorners);
+                } else
+                {
+                    labelWarningCorners.Text = "Вводимое число должно быть в диапазоне от 3 до 10, по умолчанию установлено 5";
+                    labelWarningCorners.Visible = true;
+                    values.Insert(0, valueRadius);
+                    values.Insert(1, "5");
+                }
+            }
+            else
+            {
+                labelWarningRadius.Text = "Вводимое число не должно быть не больше 10, по умолчанию установлен радиус 1";
+                labelWarningRadius.Visible = true;
+                labelWarningCorners.Text = "Вводимое число должно быть в диапазоне от 3 до 10, по умолчанию установлено 5";
+                labelWarningCorners.Visible = true;
+                values.Insert(0, "1");
+                values.Insert(1, "5");
+            }
+            return values;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            pictureBox.Width = (int)(this.Width - panel1.Width);
+            pictureBox.Height = (int)(this.Width - panel1.Width);
+            
+            DrawStarInCircle(pictureBox, pictureBox.Width/2-30, int.Parse(GetValues()[1]));
+        }
+        private void textBoxRadius_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != '\b')  // Разрешаем ввод только цифр и символа "Backspace"
+            {
+                e.Handled = true; // Отменяем ввод символа
+                labelWarningRadius.Text = "Допускается ввод только целых неотрицательных чисел";
+                labelWarningRadius.Visible = true;
+            }
+            else
+            {
+                labelWarningRadius.Visible = false;
+            }
+        }
+
+        private void textBoxNumCorners_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != '\b')  // Разрешаем ввод только цифр и символа "Backspace"
+            {
+                e.Handled = true; // Отменяем ввод символа
+                labelWarningCorners.Text = "Допускается ввод только целых неотрицательных чисел";
+                labelWarningCorners.Visible = true;
+            }
+            else
+            {
+                labelWarningCorners.Visible = false;
+            }
+        }
+        private void pictureBox_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
